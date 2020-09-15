9b79329ff5d867f4e77839ff6f6f78d62f98a482
OlaTelloo <66441031+OlaTelloo@users.noreply.github.com>
2020-09-15 22:08:16 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,76 @@
-test <- read.table("getdata_projectfiles_UCI HAR Dataset/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+# Install the necessary Packages
+library(data.table)
+library(dplyr)
+library(stringr)
 
-train <- read.table("getdata_projectfiles_UCI HAR Dataset/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+features <-  read.table("getting and cleaning data/UCI HAR Dataset/features.txt") %>%
+  rename(n = V1,
+         functions = V2)
 
-col_names <- read.table("getdata_projectfiles_UCI HAR Dataset/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt") %>%
-  select(V2) %>%
-  rename(Measurement = V2)
-
-df_all <- rbind.data.frame(test, train)
-
-Mean <- c()
-Std <- c()
-Average <- c()
-
-for (i in 1:561) {
-  Mean_value <- mean(df_all[,i])
-  Mean <- c(Mean, Mean_value)
-
-  Std_value <- sd(df_all[,i])
-  Std <- c(Std, Std_value)
-  
-  Average_value <- sd(df_all[,i])
-  Average <- c(Average, Average_value)
-  }
-
-output <- cbind.data.frame(col_names, Mean, Std, Average)
+activities <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt") %>%
+  rename(code = V1,
+         activity = V2)
 
 
-write.table(output,
-            "Analysis.txt",
-            row.names = F)
+
+X_train <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",
+                      col.names = features$functions) 
+
+
+Y_train <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt") %>%
+  rename("code" = V1)
+
+
+Sub_train <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt") %>%
+  rename("subject" = V1)
+
+X_test <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt",
+                     col.names = features$functions)
+
+Y_test <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt") %>%
+  rename("code" = V1)
+
+Sub_test <- read.table("getdata_projectfiles_UCI HAR Dataset_new/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt") %>%
+  rename("subject" = V1)
+
+X_df <- rbind(X_train, X_test)
+Y_df <- rbind(Y_train, Y_test)
+
+Subject <- rbind(Sub_test, Sub_train)
+All_data <- cbind(Subject, Y_df, X_df)
+
+
+TidyData <- All_data %>%
+  select(subject,
+         code,
+         contains("mean"),
+         contains("std")) %>%
+  left_join(.,
+            activities,
+            by = "code")
+
+TidyData <- TidyData %>%
+  mutate(code = activity) %>%
+  select(-activity)
+
+
+#renamed the data set's columns with descriptive variable names
+names(TidyData) <- case_when(str_detect(names(TidyData),"Acc" ) ~ gsub("Acc", "Accelerometer", names(TidyData)),
+                             str_detect(names(TidyData),"Gyro" ) ~ gsub("Gyro", "Gyroscope", names(TidyData)),
+                             str_detect(names(TidyData),"BodyBody" ) ~ gsub("BodyBody", "Body", names(TidyData)),
+                             str_detect(names(TidyData),"Mag" ) ~ gsub("Mag", "Magnitude", names(TidyData)),
+                             str_detect(names(TidyData),"^t" ) ~ gsub("^t", "Time", names(TidyData)),
+                             str_detect(names(TidyData),"^f" ) ~ gsub("^f", "Frequency", names(TidyData)),
+                             str_detect(names(TidyData),"tBody" ) ~ gsub("tBody", "TimeBody", names(TidyData)),
+                             str_detect(names(TidyData),"-mean()" ) ~ gsub("-mean()", "Mean", names(TidyData)),
+                             str_detect(names(TidyData),"-std()" ) ~ gsub("-std()", "STD", names(TidyData)),
+                             str_detect(names(TidyData),"-freq()" ) ~ gsub("-freq()", "Frequency", names(TidyData)),
+                             str_detect(names(TidyData),"angle" ) ~ gsub("angle", "Angle", names(TidyData)),
+                             str_detect(names(TidyData),"gravity" ) ~ gsub("gravity", "Gravity", names(TidyData)),
+                             TRUE ~ names(TidyData))
+
+#Step 4: creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+Output <- TidyData %>% 
+  group_by(subject, code) %>%
+  summarise_all(funs(mean))
+
